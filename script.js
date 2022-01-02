59986da1f8f5e9aae99addb96f0ca62ebdb72727
Jairfsj <jairfsj@gmail.com>
2022-01-02 00:17:19 -0300

    modified : scripts

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-function myFunction(x,y) {
-   alert(x + y);
-}
+var x = 7;
 
-myFunction(9,0);
+if(x >= 8){
+    alert("Sim, x é maior ou igual a 8");
+}else{
+    alert("Não, x é menor que  8");
+}

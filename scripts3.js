bc6dd1982084e01ac2740282192c8fccd035b4f1
Jairfsj <jairfsj@gmail.com>
2022-01-22 22:14:17 -0300

    more  : scripts

---
+++ scripts3.js
@@ -0,0 +1,29 @@
+let nome = 'Jair';
+let idade = 38;
+let estaAprovado = true;
+let sobrenome = undefined;
+let nCasa = 431;
+let estaEmCasa = true;
+let nomeMae ='Iracy';
+
+
+
+
+
+// key_value_pair for create 'OBJECT' ;
+
+let pessoa = { 
+    nome: 'Jair',
+    idade: 38,
+    estaAprovado: true,
+    sobrenome:'Da silva júnior.',
+    nCasa: 431,
+    estaEmCasa: true,
+    nomeMae: 'Iracy',
+
+};
+
+console.log(pessoa);
+
+
+

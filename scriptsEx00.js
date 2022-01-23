adfc2deaeae08189bcf86fe0e0ae0f78c63de843
Jairfsj <jairfsj@gmail.com>
2022-01-22 22:19:25 -0300

    more  : scripts

---
+++ scriptsEx00.js
@@ -0,0 +1,27 @@
+//Escreva uma funçãa que use 2 números e retorna o maior entre eles .
+//let valorMaior = max(10,10);
+//console.log(valorMaior);
+
+//function max(number1,number2){
+//    if(number1 > number2)
+//         return number1;
+//    else return number2  
+//}
+
+//let valorMaior = max(23,2);
+//console.log(valorMaior);
+
+//function max(number1,number2){
+//    if(number1 > number2)
+//         return number1;
+//     return number2; 
+
+//} 
+
+
+let valorMaior = max(23,2);
+console.log(valorMaior);
+
+function max(number1,number2){
+    return number1 > number2 ? number1: number2 ; 
+}

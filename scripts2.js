0d693349afe146f11f0e3549c4c64abbc548c6c2
Jairfsj <jairfsj@gmail.com>
2022-01-22 22:07:24 -0300

    more  : scripts

---
+++ scripts2.js
@@ -0,0 +1,6 @@
+//tipos de referẽncia;
+//let name = 'Jair'; //string literal;
+//let idade = 25; //number literal;
+//let estaAprovado = true; //boolean;
+//let sobrenome = undefined; //'undefined' no recomended;
+//let corSelecionado = null // redefined;

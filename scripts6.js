8ba9f7faff7afdb6ebddbbdeaf12d12c945735c8
Jairfsj <jairfsj@gmail.com>
2022-01-22 22:16:58 -0300

    more  : scripts

---
+++ scripts6.js
@@ -0,0 +1,20 @@
+//tipos de functions ;
+//Realizar um tarefa, não devolve nada;
+
+function dizerNome(){
+    console.log('Jair');
+}
+
+dizerNome();
+
+function MultiplicarPorDois(valor){
+    return valor* 2;
+}
+
+//console.log(MultiplicarPorDois(5));
+
+let resultado = MultiplicarPorDois(5);
+ 
+console.log(resultado);
+
+

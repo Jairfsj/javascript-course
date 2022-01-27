39cf9ffef33f00c9551cd4490cb88908b2646d7f
Jairfsj <jairfsj@gmail.com>
2022-01-26 21:43:50 -0300

    new : scripts

---
+++ scriptsO10.js
@@ -0,0 +1,21 @@
+//SswitchCase;
+
+let  permissao; // comum, gerente,diretor;
+permissao = 'diretor';
+switch (permissao) {
+    case 'comum':
+    console.log('Usuário comum');
+    break;
+
+    case 'gerente':
+    console.log('Usuário gerente');
+    break;
+
+    case 'diretor':
+    console.log('Usuário diretor');
+    break;
+
+    default:
+    console.log('Usuário não reconhecido!');
+}
+

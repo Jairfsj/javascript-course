a251b6d2853d9ce0b185da9058a0e6656c739e11
Jairfsj <jairfsj@gmail.com>
2022-01-26 21:45:37 -0300

    new : scripts

---
+++ scriptsO12.js
@@ -0,0 +1,26 @@
+//1.For;
+
+//console.log('Estou aprendendo!');
+//console.log('Estou aprendendo!');
+//console.log('Estou aprendendo!');
+//console.log('Estou aprendendo!');
+//console.log('Estou aprendendo!');
+
+
+
+//for(let i = 1; i <= 5; i++) {
+//    console.log('Estou aprendendo!',i);
+//}
+
+
+//for(let i = 1; i <= 5; i++){
+//    if(i % 2 !== 0){
+//        console.log(i,'Estou aprendendo!');
+   // } //modulus operator search;
+//}
+
+for(let i = 5; i >= 1; i--){
+    if(i % 2 !== 0){
+        console.log(i); //maior pra menor;
+    }
+}

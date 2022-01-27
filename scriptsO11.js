9afd1e9958e859d9b61d6dfb5052611f2056d908
Jairfsj <jairfsj@gmail.com>
2022-01-26 21:44:32 -0300

    new : scripts

---
+++ scriptsO11.js
@@ -0,0 +1,11 @@
+// console.log('Estou aprendendo!');
+// console.log('Estou aprendendo!');
+// console.log('Estou aprendendo!');
+// console.log('Estou aprendendo!');
+// console.log('Estou aprendendo!');
+
+//1.for;
+//2.While;
+//3.Do..while;
+//For..In;
+//5.For..of;
